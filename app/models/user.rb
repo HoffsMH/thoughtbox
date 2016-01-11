df09683ea6f0632342d51ae798a7c9ac72afa5d1
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 09:59:06 -0700

    made some validations and has_secure_blah for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates_confirmation_of :password
+
 end
