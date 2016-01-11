e7dfd07f168ae46428c7639d3a1c5ec64579ce4d
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 11:13:44 -0700

    added template for link form

---
+++ spec/features/user_can_see_link_forms_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "UserCanSeeLinkForms", type: :feature do
+  context "user is already signed in" do
+    before(:each) do
+      visit "/signup"
+      fill_in('Email*', :with => 'valid@emailaddress.com')
+      fill_in('Password*', :with => 'validpassword')
+      fill_in('Confirm Password*', :with => 'validpassword')
+
+      click_link_or_button "Create Account"
+    end
+    context "and the user is on the links index" do
+      before(:each) do
+        visit "/"
+      end
+      it "can see a form to make a new idea" do
+        expect(current_path).to eq("/")
+        expect(page).to have_css("input[]")
+      end
+    end
+  end
+end
