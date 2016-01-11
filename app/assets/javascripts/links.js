2bb63010f52682f82fd00880b14b0d07098be59c
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 14:59:07 -0700

    done with base requirements now to fill in testing

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -2,19 +2,42 @@ $(document).ready(function() {
   load_ideas();
   $(document).on("click", ".mark", changeReadStatus);
   $(document).on("keyup", ".search", searchFilter);
+  $(document).on("click", ".status-filter", statusFilter);
+  $(document).on("click", ".alpha-filter", alphaFilter);
 });
+
+function alphaFilter() {
+   var sorted = $(IDEAS).sort(function (first, second) {
+    return $(first).find(".link-title").text() > $(second).find(".link-title").text();
+  });
+  $(".ideas").html("");
+  $(".ideas").append(sorted);
+}
 
 function searchFilter() {
   var value = $(this).val().toUpperCase();
    var filtered = $(IDEAS).filter(function (index, element) {
     title_content  = $(element).find(".link-title").html().toUpperCase();
-    console.log(title_content);
-    console.log(value);
     return title_content.includes(value);
   });
   $(".ideas").html("");
   $(".ideas").append(filtered);
 }
+
+function statusFilter() {
+    if ($(this).hasClass("none-filter")) {
+      $(".ideas").html("");
+      $(".ideas").append($(IDEAS));
+      return true
+    }
+   var read = $(this).hasClass("read-filter") || false;
+   var filtered = $(IDEAS).filter(function (index, element) {
+     return $(element).hasClass("read-"+read);
+  });
+  $(".ideas").html("");
+  $(".ideas").append(filtered);
+}
+
 
 function changeReadStatus() {
   var id = $(this).closest(".idea").attr("id");
