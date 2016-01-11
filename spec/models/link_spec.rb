4c184624c5d33a9c02a7ec90ab39ec10b5147367
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 11:37:59 -0700

    we are now testing validation on link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  let(:link) do
+    Link.new(url: "http://validurl.com", title: "validtitle")
+  end
 
+  it "is valid" do
+    expect(link).to be_valid
+  end
+
+  it "is invalid with out an title" do
+    link.title = nil
+    expect(link).not_to be_valid
+  end
+
+  it "is invalid without a url" do
+    link.url = nil
+    expect(link).not_to be_valid
+  end
+
+  it "is invalid without a valid url" do
+    link.url = "lkjasdf"
+    expect(link).not_to be_valid
+  end
 end
