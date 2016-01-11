29078f8d6aef185be124d1170d6d3bdad546d0f1
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 11:03:57 -0700

    we can now sign out

--- spec/features/user_can_logouts_spec.rb
+++ spec/features/user_can_logouts_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanLogouts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context "user is already signed in" do
+    before(:each) do
+      visit "/signup"
+      fill_in('Email*', :with => 'valid@emailaddress.com')
+      fill_in('Password*', :with => 'validpassword')
+      fill_in('Confirm Password*', :with => 'validpassword')
+
+      click_link_or_button "Create Account"
+    end
+    context "and the user is on the links index" do
+      before(:each) do
+        visit "/"
+      end
+      it "can click sign up and be brought to signup page" do
+        expect(current_path).to eq("/")
+
+        click_link_or_button("Sign Out")
+        expect(current_path).to eq("/login")
+      end
+    end
+  end
 end
