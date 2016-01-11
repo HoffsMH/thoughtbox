17efb8d076ab13dde03b8fc99b7009961623b5dc
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 10:01:44 -0700

    added validation testing for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.new(email: "joe@example.com", password: "pass")
+  end
+
+  it "is valid" do
+    expect(user).to be_valid
+  end
+
+  it "is invalid with out an email" do
+    user.email = nil
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with out unique email" do
+    user.save
+    user2 = User.create(email: "joe@example.com", password: "pass")
+    expect(user2).not_to be_valid
+  end
+
 end
