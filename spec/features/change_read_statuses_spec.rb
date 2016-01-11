af7e47dbe35eb919c1cf5649e4bf1d2b73d6f532
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 15:14:12 -0700

    ...

--- spec/features/change_read_statuses_spec.rb
+++ spec/features/change_read_statuses_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "ChangeReadStatuses", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context "user is already signed in" do
+    before(:each) do
+      visit "/signup"
+      fill_in('Email*', :with => 'valid@emailaddress.com')
+      fill_in('Password*', :with => 'validpassword')
+      fill_in('Confirm Password*', :with => 'validpassword')
+
+      click_link_or_button "Create Account"
+    end
+    context "and the user is on the links index" do
+      before(:each) do
+        visit "/"
+      end
+      context "and there is a linkg" do
+        before(:each) do
+          fill_in("Title", with: "validtitle")
+          fill_in("Link", with: "http://validlink.com")
+          click_link_or_button "Submit Link"
+        end
+        it "should be able to change the read status", js: true do
+          within(:css, ".idea") do
+            find('.mark', :text => 'mark as read').click
+            expect(page).not_to have_content("mark as read")
+            expect(page).to have_content("mark as unread")
+          end
+        end
+      end
+    end
+  end
 end
