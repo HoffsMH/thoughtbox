188340f4eba510893a5cd972fed14fb0e32a5304
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 12:24:59 -0700

    done with sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,32 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+   user = User.find_by(email: user_email)
+    if user && user.authenticate(user_password)
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now.alert = "Invalid email or password"
+      redirect_to login_path
+    end
+  end
+
   def destroy
     session.delete(:user_id)
     redirect_to login_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def user_password
+    user_params[:password]
+  end
+
+  def user_email
+    user_params[:email]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post "/links" => "links#create"
 
   get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
   get "/signup" => "users#new"
   post "/signup" => "users#create"
