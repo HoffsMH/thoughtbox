9aa794a7d30aff6a4488b5404553451c3df36d1e
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 11:09:43 -0700

    done with iteration 1 all test passing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'GET sessions#destroy' do
+    context 'logged in' do
+      before(:each) do
+        @user = User.create(email: "valid@email.com",
+                            password: "validpassword",
+                            password_confirmation: "validpassword")
+        session[:user_id] = @user.id
+      end
+      it 'redirects to session#new' do
+        get :destroy
+        expect(session[:user_id]).to eq(nil)
+        expect(response).to redirect_to("/login")
+      end
+    end
+  end
 
 end
