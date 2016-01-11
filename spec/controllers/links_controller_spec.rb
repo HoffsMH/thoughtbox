c8535b4c5f6a1c72b900dce0c6bddd0bd7aa7e77
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 09:28:08 -0700

    we have our first test and its erroring yeyeyeye

---
+++ spec/controllers/links_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe LinksController, type: :controller do
+  describe 'GET links#index' do
+    context 'not logged in' do
+      it 'redirects to session#new' do
+        get :index
+
+        expect(response).to redirect_to(location)
+      end
+    end
+  end
+
+
+end
