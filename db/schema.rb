97a49c3ced487efb669e5955c38d3bc595630ba5
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 15:19:38 -0700

    fixed test and got rid of stupid helpers

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20160111184123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ideas", force: :cascade do |t|
-    t.string   "title"
-    t.string   "url"
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
