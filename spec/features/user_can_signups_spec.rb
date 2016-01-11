133f316357d7771ff3979644fbfd70d3f1d89955
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 09:46:20 -0700

    we have basic capybara test up and working

--- spec/features/user_can_signups_spec.rb
+++ spec/features/user_can_signups_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanSignups", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context "user is not already signed in" do
+    context "and the user is on the login page" do
+      before(:each) do
+        visit "/login"
+      end
+      it "can click sign up and be brought to signup page" do
+        click_link_or_button "Sign Up"
+
+        expect(current_path).to.eq("/signup")
+      end
+    end
+  end
 end
