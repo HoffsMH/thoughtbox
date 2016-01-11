4ac4e4dd69684d1bfaed79b32a72707317c2c29a
HoffsMH <hecker.matthew@gmail.com>
2016-01-11 10:25:57 -0700

    welp we are now putting users in there

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new(new_user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to action: 'new'
+    end
+  end
+
+  private
+  def new_user_params
+    params.require(:user).permit(:email, :password,
+    :password_confirmation)
+  end
 end
